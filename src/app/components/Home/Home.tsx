3cec224cb2b2ad3eebcaab5c24c6c8288821cc5f
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2026-01-11 17:02:59 +0400

    Add Proof of Inspect sections

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -3,12 +3,14 @@ import HeroA from './HeroSection/HeroA'
 import HeroB from './HeroSection/HeroB'
 import { FAQSection } from './FAQ'
 import { FeaturedNewsSection } from './FeaturedNews'
+import ProofOfInspect from './ProofOfInspect/ProofOfInspect'
 
 function Home() {
   return (
     <div>
       <HeroA/>
       <HeroB/>
+      <ProofOfInspect/>
       
       <FeaturedNewsSection/>
       <FAQSection/>

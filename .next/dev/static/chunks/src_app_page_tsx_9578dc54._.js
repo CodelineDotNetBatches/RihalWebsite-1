17bddad6dac2aa6ae965278592b39fcfbb261a04
Hisham Al-Harrasi <hisham.harrasi@rihal.om>
2026-01-22 11:35:28 +0400

    chore: remove forgetten .next dir

--- .next/dev/static/chunks/src_app_page_tsx_9578dc54._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_fac268a1._.js",
-  "static/chunks/2069a_next_5b05c6c0._.js"
-],
-    source: "dynamic"
-});
